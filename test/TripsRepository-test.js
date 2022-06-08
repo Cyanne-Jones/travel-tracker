8aec0ac779e3ff6efb0f9feefcfd2030826c627e
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-08 14:49:17 -0600

    Add test to assert class storage of trips

--- test/TripsRepository-test.js
+++ test/TripsRepository-test.js
@@ -13,4 +13,100 @@ describe('TripsRepository', () => {
     expect(TripsRepository).to.be.a('function');
   });
 
+  it('Should hold an array of trips', () => {
+    expect(tripsRepository.trips).to.deep.equal(
+      [
+        {
+          "id": 1,
+          "userID": 1,
+          "destinationID": 3,
+          "travelers": 1,
+          "date": "2022/09/16",
+          "duration": 8,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 2,
+          "userID": 1,
+          "destinationID": 2,
+          "travelers": 2,
+          "date": "2020/09/16",
+          "duration": 6,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 3,
+          "userID": 1,
+          "destinationID": 3,
+          "travelers": 1,
+          "date": "2022/06/08",
+          "duration": 7,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 4,
+          "userID": 2,
+          "destinationID": 4,
+          "travelers": 1,
+          "date": "2022/09/16",
+          "duration": 8,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 5,
+          "userID": 2,
+          "destinationID": 5,
+          "travelers": 1,
+          "date": "2020/09/16",
+          "duration": 8,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 6,
+          "userID": 2,
+          "destinationID": 6,
+          "travelers": 1,
+          "date": "2022/07/08",
+          "duration": 7,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 7,
+          "userID": 3,
+          "destinationID": 7,
+          "travelers": 1,
+          "date": "2022/09/16",
+          "duration": 8,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 8,
+          "userID": 3,
+          "destinationID": 8,
+          "travelers": 1,
+          "date": "2020/09/16",
+          "duration": 8,
+          "status": "approved",
+          "suggestedActivities": []
+        },
+        {
+          "id": 9,
+          "userID": 3,
+          "destinationID": 9,
+          "travelers": 1,
+          "date": "2022/07/08",
+          "duration": 7,
+          "status": "approved",
+          "suggestedActivities": []
+        }
+      ]
+    );
+  })
 });
