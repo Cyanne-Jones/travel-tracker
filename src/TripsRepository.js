d9df416de2e71c9eae9eaf4b921390a0600a34a7
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-08 16:14:15 -0600

    Create getTravelerPastTrips function

--- src/TripsRepository.js
+++ src/TripsRepository.js
@@ -1,3 +1,6 @@
+import dayjs from 'dayjs';
+dayjs().format();
+
 class TripsRepository {
   constructor(trips) {
     this.trips = trips;
@@ -6,6 +9,12 @@ class TripsRepository {
   getTripsByTravelerId(idNum) {
     const travelersTrips = this.trips.filter(trip => trip.userID === idNum);
     return travelersTrips;
+  };
+
+  getTravelerPastTrips(idNum) {
+    const userTrips = this.getTripsByTravelerId(idNum);
+    const userPastTrips = userTrips.filter(trip => dayjs(trip.date).isBefore(Date.now()));
+    return userPastTrips
   };
 };
 
