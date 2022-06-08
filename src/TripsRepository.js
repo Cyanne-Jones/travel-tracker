c0ac5ef56a98704575d9b1dc8b32560aff27cc2c
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-08 15:10:34 -0600

    Add getTripsByTravelerId function

--- src/TripsRepository.js
+++ src/TripsRepository.js
@@ -2,6 +2,11 @@ class TripsRepository {
   constructor(trips) {
     this.trips = trips;
   }
+
+  getTripsByTravelerId(idNum) {
+    const travelersTrips = this.trips.filter(trip => trip.userID === idNum);
+    return travelersTrips;
+  };
 };
 
 export default TripsRepository;
