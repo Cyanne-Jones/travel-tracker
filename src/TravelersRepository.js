ac12ba830fce7ae727918ddfcc65b632192b48fd
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-08 11:29:57 -0600

    Set up test file and class file

---
+++ src/TravelersRepository.js
@@ -0,0 +1,7 @@
+class TravelersRepository {
+  constructor() {
+
+  }
+};
+
+export default TravelersRepository;
--- test/TravelersRepository-test.js
+++ test/TravelersRepository-test.js
@@ -1,7 +1,9 @@
 import chai from 'chai';
 const expect = chai.expect;
+import TravelersRepository from '../src/TravelersRepository.js';
+import travelersSampleData from '../data/travelers.js';
 
-describe('See if the tests are running', function() {
+describe('TravelersRepository', function() {
   it('should return true', function() {
     expect(true).to.equal(true);
   });
