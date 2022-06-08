2641e7e41809805be150ea1da5c3212f5fa39c3e
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-08 11:57:06 -0600

    Add getTravelerByidNum functionality

--- src/TravelersRepository.js
+++ src/TravelersRepository.js
@@ -2,6 +2,11 @@ class TravelersRepository {
   constructor(travelersData) {
     this.travelers = travelersData;
   }
+
+  getTravelerByidNum(idNum) {
+    const correctTraveler = this.travelers.find(traveler => traveler.id === idNum);
+    return correctTraveler;
+  };
 };
 
 export default TravelersRepository;
