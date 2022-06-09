860639f4c67ce3e17d9c2b4c31d685750f6b750b
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-09 12:52:33 -0600

    Set up scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,13 @@
 import './css/styles.css';
+import TravelersRepository from './TravelersRepository';
+import DestinationRepository from './DestinationRepository';
+import TripsRepository from './TripsRepository';
+import Traveler from './Traveler';
+import {fetchApiData} from './apiCalls.js';
+
+//GLOBAL VARIABLES
+
+var travelersRepo;
+var destinationRepo;
+var tripsRepo;
+var traveler;
