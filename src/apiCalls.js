774b79b5956698a054bc6579e1987bd44296229f
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-10 10:15:26 -0600

    Add postNewTrip function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,38 @@
+import { errorMessage, addTripToPage } from './scripts.js';
+
 function fetchApiData(url) {
   return fetch(url)
       .then(promise => promise.json())
 };
 
-export {fetchApiData};
+function postNewTrip(newTrip) {
+  fetch('http://localhost:3001/api/v1/trips', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newTrip)
+  })
+  .then(response => {
+    checkForError(response)
+  })
+  .then(trip => {
+    console.log(trip);
+    return addTripToPage(trip)})
+  .catch(error => {
+    console.log('ERROR', error)
+    if (error.message === 'Failed to fetch') {
+      errorMessage.innerText = 'Failed to fetch new data, pleasae start server.';
+    } else {
+      errorMessage.innerText = error.message;
+    }
+  })
+};
+
+const checkForError = (response) => {
+  if (response.status === 422) {
+    throw new Error(`You haven't entered all the information, please try again.`)
+  } else {
+    return response.json()
+  }
+};
+
+export {fetchApiData, postNewTrip, checkForError};
